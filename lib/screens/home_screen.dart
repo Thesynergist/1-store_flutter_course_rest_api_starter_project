0147cafbb2ff0daf2833d48469443af9e3737b90
Fritz Marsden <marsdenfritz@gmail.com>
2024-01-12 22:40:35 +0000

    First commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,16 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:store_api_flutter_course/consts/global_colors.dart';
+import 'package:store_api_flutter_course/screens/categories_screen.dart';
+import 'package:store_api_flutter_course/screens/feeds_screen.dart';
+import 'package:store_api_flutter_course/widget/appbar_icons.dart';
+import 'package:store_api_flutter_course/widget/category_widget.dart';
+import 'package:store_api_flutter_course/widget/feeds_widget.dart';
+import 'package:store_api_flutter_course/widget/sale_widget.dart';
+import 'package:page_transition/page_transition.dart';
+
+//TODO: once this is done practice making some designs from dribble, with provider state management
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,15 +20,142 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late TextEditingController _textEditingController;
+  @override
+  void initState() {
+    _textEditingController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
       },
-      child: const Scaffold(
-        body: Center(child: Text("Welcome to this course")),
-      ),
+      child: Scaffold(
+          appBar: AppBar(
+            // elevation: 4,
+            title: const Text('Home'),
+            leading: AppBarIcons(
+              function: () {
+                Navigator.push(
+                  context,
+                  PageTransition(
+                      child: const CategoriesScreen(),
+                      type: PageTransitionType.fade),
+                );
+              },
+              icon: IconlyBold.category,
+            ),
+            actions: [
+              AppBarIcons(
+                function: () {},
+                icon: IconlyBold.user3,
+              ),
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 18,
+                ),
+                TextField(
+                  controller: _textEditingController,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                      hintText: "Search",
+                      filled: true,
+                      fillColor: Theme.of(context).cardColor,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(
+                          color: Theme.of(context).cardColor,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
+                      ),
+                      suffixIcon: Icon(
+                        IconlyLight.search,
+                        color: lightIconsColor,
+                      )),
+                ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(children: [
+                      SizedBox(
+                        height: size.height * 0.25,
+                        child: Swiper(
+                          itemCount: 3,
+                          itemBuilder: (ctx, index) {
+                            return const SaleWidget();
+                          },
+                          autoplay: true,
+                          pagination: const SwiperPagination(
+                              alignment: Alignment.bottomCenter,
+                              builder: DotSwiperPaginationBuilder(
+                                  color: Colors.white,
+                                  activeColor: Colors.red)),
+                          // control: const SwiperControl(),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            const Text(
+                              "Latest Products",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 18,
+                              ),
+                            ),
+                            const Spacer(),
+                            AppBarIcons(
+                                function: () {
+                                  Navigator.push(
+                                      context,
+                                      PageTransition(
+                                          type: PageTransitionType.fade,
+                                          child: const FeedsScreen()));
+                                },
+                                icon: IconlyBold.arrowRight2),
+                          ],
+                        ),
+                      ),
+                      GridView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: 3,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2,
+                                  crossAxisSpacing: 0.0,
+                                  mainAxisSpacing: 0.0,
+                                  childAspectRatio: 0.6),
+                          itemBuilder: (ctx, index) {
+                            return const FeedsWidget();
+                          })
+                    ]),
+                  ),
+                )
+              ],
+            ),
+          )),
     );
   }
 }
