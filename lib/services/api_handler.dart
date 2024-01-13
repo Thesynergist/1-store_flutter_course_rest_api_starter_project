fd075e135b4f0eda709387f1e79175aa85061857
Fritz Marsden <marsdenfritz@gmail.com>
2024-01-13 17:56:28 +0000

    error handling complete but not working

--- lib/services/api_handler.dart
+++ lib/services/api_handler.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:developer';
 //https://javiercbk.github.io/json_to_dart/
 
 import 'package:http/http.dart' as http;
@@ -10,7 +11,8 @@ import 'package:store_api_flutter_course/models/users_models.dart';
 class APIHandler {
 
   static Future<List<dynamic>> getData({required String target}) async {
-    var uri = Uri.https(BASE_URL,
+   try {
+     var uri = Uri.https(BASE_URL,
         "/api/v1/$target"); //TODO: Why is this better than Uri.parse()
     var response = await http.get(
       uri,
@@ -22,11 +24,18 @@ class APIHandler {
     var data = jsonDecode(response.body);
 
     List tempList = [];
+    if(response.statusCode != 200) {
+      throw data['message'];
+    }
     for (var v in data) { //TODO: This sorts through each item in the list, which are product maps
       tempList.add(v);
       //print("V $v \n\n");
     }
     return tempList;
+   } catch (error) {
+    log('An error occured $error');
+    throw error.toString(); //TODO: what is throw
+   }
   }
 
   static Future<List<ProductsModel>> getAllProducts() async {
@@ -65,7 +74,8 @@ class APIHandler {
 
 
   static Future<ProductsModel> getProductById({required String id}) async {
-    var uri = Uri.https(BASE_URL,
+    try {
+      var uri = Uri.https(BASE_URL,
         "/api/v1/products/$id"); //TODO: Why is this better than Uri.parse()
     var response = await http.get(
       uri,
@@ -75,8 +85,14 @@ class APIHandler {
     //   'response: ${jsonDecode(response.body)}',
     // );
     var data = jsonDecode(response.body);
-
+    // if(response != 200) {  //The product details page doesnt load when this is used.
+    //   throw data['message'];
+    // }
    
     return ProductsModel.fromJson(data);
-  }
+    } catch (e) {
+      log('An error occured while getting produt info $e');
+      throw e.toString();
+    }
+  } 
 }
